26e6d59afedb1df5e01b83785154a9fbf6163d05
ican0422 <ican0422@gmail.com>
2025-08-06 20:37:12 +0900

    git init

---
+++ src/test/java/com/careerthon/reactpractice/ReactPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerthon.reactpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
