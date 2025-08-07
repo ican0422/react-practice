3447880f351678f19196aa35eaeb3d800208628a
ican0422 <ican0422@gmail.com>
2025-08-07 22:12:35 +0900

    fix: 시간 기록 null 버그 수정

--- src/main/java/com/careerthon/reactpractice/ReactPracticeApplication.java
+++ src/main/java/com/careerthon/reactpractice/ReactPracticeApplication.java
@@ -2,8 +2,10 @@ package com.careerthon.reactpractice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ReactPracticeApplication {
 
 	public static void main(String[] args) {
