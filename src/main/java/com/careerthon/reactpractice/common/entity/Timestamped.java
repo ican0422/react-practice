1386b3df61bcdaa87f0e1a85d6e076c560b75501
ican0422 <ican0422@gmail.com>
2025-08-07 20:07:15 +0900

    feat: Timestamped 추가

---
+++ src/main/java/com/careerthon/reactpractice/common/entity/Timestamped.java
@@ -0,0 +1,27 @@
+package com.careerthon.reactpractice.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@MappedSuperclass // Timestamped 엔티티를 상속하는 객체에 자동으로 timestamped 필드의 컬럼 부여.
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Timestamped {
+    @CreatedDate
+    @Column(updatable = false)
+    private Date created;
+
+    @LastModifiedDate
+    @Column
+    private Date modified;
+}
