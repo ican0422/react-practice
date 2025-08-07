5692333e066eccab67542948cf7b96cc26a0df4f
ican0422 <ican0422@gmail.com>
2025-08-07 21:46:25 +0900

    feat: 생성자를 통한 필드 초기화

--- src/main/java/com/careerthon/reactpractice/domain/user/dto/respons/UserSignupResponseDto.java
+++ src/main/java/com/careerthon/reactpractice/domain/user/dto/respons/UserSignupResponseDto.java
@@ -1,12 +1,13 @@
 package com.careerthon.reactpractice.domain.user.dto.respons;
 
+import com.careerthon.reactpractice.domain.user.entity.User;
 import lombok.Getter;
 
 @Getter
 public class UserSignupResponseDto {
     private final String userId;
 
-    public UserSignupResponseDto(String userId) {
-        this.userId = userId;
+    public UserSignupResponseDto(User user) {
+        this.userId = user.getUserId();
     }
 }
