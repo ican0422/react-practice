9364bef171622b9daab7f408d3b68231ed9a02b0
ican0422 <ican0422@gmail.com>
2025-08-07 20:08:36 +0900

    feat: User 엔티티 추가

---
+++ src/main/java/com/careerthon/reactpractice/domain/user/entity/User.java
@@ -0,0 +1,23 @@
+package com.careerthon.reactpractice.domain.user.entity;
+
+import com.careerthon.reactpractice.common.entity.Timestamped;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "User")
+public class User extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String username;
+    private String nickname;
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole userRole;
+}
