33f2b604f3a7fb0ae298316b08ae35bcb4398bc2
ican0422 <ican0422@gmail.com>
2025-08-08 01:16:59 +0900

    fix: BaseExceptionConst 인터페이스 적용

--- src/main/java/com/careerthon/reactpractice/domain/user/exception/UserExceptionConst.java
+++ src/main/java/com/careerthon/reactpractice/domain/user/exception/UserExceptionConst.java
@@ -1,12 +1,13 @@
 package com.careerthon.reactpractice.domain.user.exception;
 
+import com.careerthon.reactpractice.common.exception.BaseExceptionConst;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
-public enum UserExceptionConst {
+public enum UserExceptionConst implements BaseExceptionConst {
     // 이미 가입된 유저, 409
     DUPLICATE_USER(HttpStatus.CONFLICT),
     // 회원 정보를 찾을 수 없습니다, 404
