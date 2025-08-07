9ef4fc8cb4533bdf0578112b4c29be78a7c6b2c2
ican0422 <ican0422@gmail.com>
2025-08-07 21:47:45 +0900

    feat: JPA 쿼리 메서드 추가

--- src/main/java/com/careerthon/reactpractice/domain/user/repository/UserRepository.java
+++ src/main/java/com/careerthon/reactpractice/domain/user/repository/UserRepository.java
@@ -4,6 +4,11 @@ import com.careerthon.reactpractice.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUserId(String userId);
+
+    Optional<User> findByUserId(String userId);
 }
