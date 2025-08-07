85313f54927bda88f1849b4fb657b664d65031f7
ican0422 <ican0422@gmail.com>
2025-08-07 21:50:37 +0900

    feat: 회원가입, 로그인 컨트롤러 구현

--- src/main/java/com/careerthon/reactpractice/domain/user/controller/UserController.java
+++ src/main/java/com/careerthon/reactpractice/domain/user/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.careerthon.reactpractice.domain.user.controller;
 
+import com.careerthon.reactpractice.common.advice.ApiResponse;
+import com.careerthon.reactpractice.domain.user.dto.request.UserLoginRequestDto;
+import com.careerthon.reactpractice.domain.user.dto.request.UserSignupRequestDto;
+import com.careerthon.reactpractice.domain.user.dto.respons.UserLoginResponseDto;
+import com.careerthon.reactpractice.domain.user.dto.respons.UserSignupResponseDto;
 import com.careerthon.reactpractice.domain.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -13,8 +17,14 @@ public class UserController {
     private final UserService service;
 
     @PostMapping("/signup")
-    public void signup() {}
+    public ResponseEntity<ApiResponse<UserSignupResponseDto>> signup(@RequestBody UserSignupRequestDto userSignupRequestDto) {
+        UserSignupResponseDto user = service.signup(userSignupRequestDto);
+        return ResponseEntity.ok(ApiResponse.success("회원가입 성공", user));
+    }
 
     @PostMapping("/login")
-    public void login() {}
+    public ResponseEntity<ApiResponse<UserLoginResponseDto>> login(@RequestBody UserLoginRequestDto userLoginRequestDto) {
+        UserLoginResponseDto token = service.login(userLoginRequestDto);
+        return ResponseEntity.ok(ApiResponse.success("로그인 성공", token));
+    }
 }
