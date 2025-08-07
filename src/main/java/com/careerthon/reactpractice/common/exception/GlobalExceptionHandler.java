e78a5ec081af901e7d2bd8abddb434a3be6156a8
ican0422 <ican0422@gmail.com>
2025-08-08 01:51:22 +0900

    fix: Data를 유동적으로 넣고 뺄 수 있게 수정

---
+++ src/main/java/com/careerthon/reactpractice/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.careerthon.reactpractice.common.exception;
+
+import com.careerthon.reactpractice.common.advice.ApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<ApiResponse<?>> handleGlobalException(GlobalException e) {
+        return ResponseEntity.status(e.getExceptionConst().getHttpStatus())
+                .body(ApiResponse.error(e.getExceptionConst().name(), e.getMessage(), e.getData()));
+    }
+}
