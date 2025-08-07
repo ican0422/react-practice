09e87b67e385bed957cd016dc5ae4c610402b1d9
ican0422 <ican0422@gmail.com>
2025-08-08 00:51:42 +0900

    feat: User 커스텀 예외처리 HTTP 상태코드 Enum 형식으로 구현

---
+++ src/main/java/com/careerthon/reactpractice/domain/user/exception/UserExceptionConst.java
@@ -0,0 +1,18 @@
+package com.careerthon.reactpractice.domain.user.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum UserExceptionConst {
+    // 이미 가입된 유저, 409
+    DUPLICATE_USER(HttpStatus.CONFLICT),
+    // 회원 정보를 찾을 수 없습니다, 404
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND),
+    // 잘못된 비밀번호 입니다, 401
+    INVALID_PASSWORD(HttpStatus.UNAUTHORIZED);
+
+    private final HttpStatus httpStatus;
+}
