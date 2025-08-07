0c372d02891271f565cfdd28e39c8ccfd6b30deb
ican0422 <ican0422@gmail.com>
2025-08-07 21:48:12 +0900

    feat: 회원가입, 로그인 서비스 구현

--- src/main/java/com/careerthon/reactpractice/domain/user/service/UserService.java
+++ src/main/java/com/careerthon/reactpractice/domain/user/service/UserService.java
@@ -1,11 +1,75 @@
 package com.careerthon.reactpractice.domain.user.service;
 
+import com.careerthon.reactpractice.common.config.JwtUtils;
+import com.careerthon.reactpractice.common.config.PasswordEncoder;
+import com.careerthon.reactpractice.domain.user.dto.request.UserLoginRequestDto;
+import com.careerthon.reactpractice.domain.user.dto.request.UserSignupRequestDto;
+import com.careerthon.reactpractice.domain.user.dto.respons.UserLoginResponseDto;
+import com.careerthon.reactpractice.domain.user.dto.respons.UserSignupResponseDto;
+import com.careerthon.reactpractice.domain.user.entity.User;
+import com.careerthon.reactpractice.domain.user.entity.UserRole;
 import com.careerthon.reactpractice.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
     private final UserRepository userRepo;
+    private final JwtUtils jwtUtils;
+    private final PasswordEncoder passwordEncoder;
+
+    /* 회원가입 */
+    @Transactional
+    public UserSignupResponseDto signup(UserSignupRequestDto requestDto) {
+        // 가입 됐는지 검증
+        if(userRepo.existsByUserId(requestDto.getUserId())) {
+            throw new IllegalArgumentException("이미 가입된 유저 입니다.");
+        }
+
+        // 비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(requestDto.getPassword());
+
+        // 엔티티로 바꾼다.
+        User user = User.builder()
+                .userId(requestDto.getUserId())
+                .username(requestDto.getUsername())
+                .nickname(requestDto.getNickname())
+                .password(encodedPassword)
+                .userRole(UserRole.ROLE_USER)
+                .build();
+
+        // DB 저장
+        User saveUser = userRepo.save(user);
+
+        // 리턴
+        return new UserSignupResponseDto(saveUser);
+    }
+
+    /* 로그인 */
+    @Transactional(readOnly = true)
+    public UserLoginResponseDto login(UserLoginRequestDto requestDto) {
+        // 회원가입 되어 있는지 확인 -> 가입되어 있으면 정보를 가져온다.
+        User user = userRepo.findByUserId(requestDto.getUserId())
+                .orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
+
+        // 비밀번호 확인
+        boolean verify = passwordEncoder.verify(requestDto.getPassword(), user.getPassword());
+        if(!verify) {
+            throw new IllegalArgumentException("잘못 된 비밀번호 입니다.");
+        }
+
+        // 토큰 생성
+        String token = jwtUtils.createToken(
+                user.getId(),
+                user.getUserId(),
+                user.getUsername(),
+                user.getNickname(),
+                user.getUserRole()
+        );
+
+        // 토큰 반환
+        return new UserLoginResponseDto(token);
+    }
 }
