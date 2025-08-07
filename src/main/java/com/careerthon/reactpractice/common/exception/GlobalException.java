31f6a1279b077d5e8e61715d45f835f52dd4ee96
ican0422 <ican0422@gmail.com>
2025-08-08 01:16:05 +0900

    feat: 애플리케이션 전역에서 사용하는 공통 예외 클래스

---
+++ src/main/java/com/careerthon/reactpractice/common/exception/GlobalException.java
@@ -0,0 +1,15 @@
+package com.careerthon.reactpractice.common.exception;
+
+import lombok.Getter;
+
+// 애플리케이션 전역에서 사용하는 공통 예외 클래스
+// 각 도메인 예외는 GlobalException을 상속함으로써 전역 핸들러에서 일관된 방식으로 처리
+@Getter
+public class GlobalException extends RuntimeException {
+    private final BaseExceptionConst exceptionConst;
+
+    public GlobalException(String message, BaseExceptionConst exceptionConst) {
+        super(message);
+        this.exceptionConst = exceptionConst;
+    }
+}
